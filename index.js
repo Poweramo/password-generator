5cc17bce254874f677ec6c420074ba166983e7f4
Poweramo <assirouomar@gmail.com>
2023-10-12 11:57:06 +0000

    build[Password generating logic]: adds password generator

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
+// TODO: Add a security checker that checks the security of the generated password
+// TODO: Adding the copy password option
+
 const passwordItem = document.getElementById("password");
 const passwordLengthItem = document.getElementById("password-length");
+const passwordLengthContainerItem = document.getElementById("password-length-container");
+const lowerCase = document.getElementById("lowercase");
+const upperCase = document.getElementById("uppercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
+const generatePasswordButton = document.getElementById("generate");
+const dataLowerCase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUpperCase = dataLowerCase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = `²&é"'(-è_çà)=$*ù^!:;,/.§?%µ£¨+°\`}]@^|[{#~¤`;
 
-passwordItem.textContent = passwordLengthItem.value;
+const generatePassword = () => {
+	let password = "";
+	let data = [];
+
+	if (lowerCase.checked) data.push(...dataLowerCase);
+	if (upperCase.checked) data.push(...dataUpperCase);
+	if (numbers.checked) data.push(...dataNumbers);
+	if (symbols.checked) data.push(...dataSymbols);
+
+	if (data.length === 0) {
+		alert("You haven't chosen any option!");
+		return;
+	}
+
+	for (let i = 0; i < Number(passwordLengthItem.value); i++) {
+		password += data[Math.floor(Math.random() * data.length)];
+	}
+
+	passwordItem.textContent = password;
+};
+
+passwordLengthContainerItem.textContent = passwordLengthItem.value;
 
 passwordLengthItem.addEventListener("input", () => {
-	passwordItem.textContent = passwordLengthItem.value;
+	passwordLengthContainerItem.textContent = passwordLengthItem.value;
+});
+
+generatePasswordButton.addEventListener("click", () => {
+	generatePassword();
 });
